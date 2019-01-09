0569f2cea7117637aa845efbe5057b3620a97bf5
kaeus <kaeus3@gmail.com>
2019-01-08 22:31:23 -0500

    Firebase

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const admin = require('firebase-admin');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var serviceAccount = require('starmourn-map-data-ddb16ef243c4.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/specials', (req, res) => {
+  db.collection('room-notes')
+    .get()
+    .then((snapshot) => {
+        res.json(snapshot);
+    });
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
