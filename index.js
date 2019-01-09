98756d19b53547ff74ef7ed94054b15c53ac4d84
kaeus <kaeus3@gmail.com>
2019-01-09 00:44:38 -0500

    API works

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const url = require('url');
+const atob = require('atob');
+const moment = require('moment');
 
 const admin = require('firebase-admin');
+
+const FAIL_RESP = { success: false };
+const SUCCESS_RESP = { success: true };
 
 var serviceAccount = require('./starmourn-map-data-ddb16ef243c4.json');
 
@@ -13,30 +17,46 @@ admin.initializeApp({
 
 var db = admin.firestore();
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/special', (req, res) => {
-  db.collection('specials').get().then((snapshot) => {
-      var specials = []
-      if (snapshot.empty) {
-        res.json([]);
-      } else {
-        snapshot.forEach(doc => {
-          specials.push(doc.data());
-        });
+app.get('/getEntries', (req, res) => {
+  if (req.query.collection == null) { res.json(FAIL_RESP); return; }
 
-        res.json(specials);
-      }
+  db.collection(req.query.collection).get().then((snapshot) => {
+    var items = [];
+    if (!snapshot.empty) {
+      snapshot.forEach(doc => {
+        items.push(doc.data());
+      });
+    }
+    res.json(items)
   });
 });
 
-app.get('/uploadSpecial', (req, res) => {
+function uploadJsonData(collection, data) {
+  var ref = db.collection(collection).doc(data.key);
+  ref.set(data);
+
+  return true;
+}
+
+app.get('/uploadEntry', (req, res) => {
+  if (req.query.collection == null) { res.json(FAIL_RESP); return; }
+  if (req.query.payload == null) { res.json(FAIL_RESP); return; }
+
+  var collection = req.query.collection
   var data = atob(req.query.payload);
   var jsonData = JSON.parse(data);
 
-  var ref = db.collection('specials').doc(jsonData.key);
-  ref.set(jsonData);
+  if (jsonData.list) {
+    jsonData.list.forEach(item => {
+      var ref = db.collection(collection).doc(item.key);
+      ref.set(item);
+    });
+  } else {
+    var ref = db.collection(collection).doc(data.key);
+    ref.set(data);
+  }
 
-  res.send(200);
+  res.json(SUCCESS_RESP);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
